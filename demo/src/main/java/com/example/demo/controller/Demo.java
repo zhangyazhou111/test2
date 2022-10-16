4bc82eafe869357cf21d7c74e019ab251127d671
zhangyazhou <1047673189@qq.com>
2022-10-16 15:53:40 +0800

    demo xiu3

--- demo/src/main/java/com/example/demo/controller/Demo.java
+++ demo/src/main/java/com/example/demo/controller/Demo.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import lombok.Data;
+
+@Data
 public class Demo {
 
     private  String name;
     private  String  age;
-
-
+    private   String tall;
+    private  int     data;
 }
